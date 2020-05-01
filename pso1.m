4b1c8f59635e48a89aef1b897d3e72ff2edfd04a
xiaoke <46713142+xiaokew@users.noreply.github.com>
2020-05-01 10:12:42 +0800

    Add files via upload

---
+++ pso1.m
@@ -0,0 +1,40 @@
+
+
+clc;
+clear;
+close all;
+
+%% Problem Definiton
+
+problem.CostFunction = @(x) Sphere(x);  % Cost Function
+problem.nVar = 5;       % Number of Unknown (Decision) Variables
+problem.VarMin =  -10;  % Lower Bound of Decision Variables
+problem.VarMax =  10;   % Upper Bound of Decision Variables
+
+%% Parameters of PSO
+
+params.MaxIt = 1000;        % Maximum Number of Iterations
+params.nPop = 50;           % Population Size (Swarm Size)
+params.w = 1;               % Intertia Coefficient
+params.wdamp = 0.99;        % Damping Ratio of Inertia Coefficient
+params.c1 = 2;              % Personal Acceleration Coefficient
+params.c2 = 2;              % Social Acceleration Coefficient
+params.ShowIterInfo = true; % Flag for Showing Iteration Informatin
+
+%% Calling PSO
+
+out = PSO(problem, params);
+
+BestSol = out.BestSol;
+BestCosts = out.BestCosts;
+
+%% Results
+
+figure;
+% plot(BestCosts, 'LineWidth', 2);
+semilogy(BestCosts, 'LineWidth', 2);
+xlabel('Iteration');
+ylabel('Best Cost');
+grid on;
+
+
